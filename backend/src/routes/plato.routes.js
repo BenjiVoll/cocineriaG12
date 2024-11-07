8b2433eb4aa15ac32aa7cb34cf25f3da80f97f1c
felipesagredo <felipesagredogalindo@gmail.com>
2024-11-06 23:05:17 -0300

    update server

--- backend/src/routes/plato.routes.js
+++ backend/src/routes/plato.routes.js
@@ -1,14 +1,13 @@
 "use strict";
 import { Router } from "express";
-<<<<<<< HEAD
 import { isAdmin } from "../middlewares/authorization.middleware.js";
 import { authenticateJwt } from "../middlewares/authentication.middleware.js";
 import {
     createPlatoController,
-    updatePlatoController,
+    deletePlatoController,
     getPlatoController,
     getPlatosController,
-    deletePlatoController,
+    updatePlatoController,
 } from "../controllers/plato.controller.js";
 
 const router = Router();
@@ -22,16 +21,5 @@ router
   .get("/search/:id", getPlatosController)
   .get("/", getPlatoController)
   .delete("/delete/:id", deletePlatoController);
-=======
-import { actualizarPlatoController, 
-    crearPlatoController, 
-    obtenerPlatosDisponiblesController } from "../controllers/plato.controller.js";
-
-const router = Router();
-
-router.post("/platos",  crearPlatoController);
-router.put("/platos/:id", actualizarPlatoController);
-router.get("/platos/disponibles", obtenerPlatosDisponiblesController);
->>>>>>> rama_cocina_3
 
 export default router;
